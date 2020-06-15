19a12c6dd09f66a11423d9d07ef6ca70c3f7e73b
d0k026s <devesh.kumar@walmart.com>
2020-06-15 23:38:29 +0530

    Added new endpoint to create Customer through endpoint /create

--- src/main/java/com/devesh/mailsender/api/controller/MailController.java
+++ src/main/java/com/devesh/mailsender/api/controller/MailController.java
@@ -1,15 +1,14 @@
 package com.devesh.mailsender.api.controller;
 
-import com.devesh.mailsender.api.exception.CustomException;
+import com.devesh.mailsender.api.exception.NotFoundException;
+import com.devesh.mailsender.api.model.CustomerDto;
+import com.devesh.mailsender.api.service.CustomerCreateService;
 import com.devesh.mailsender.api.service.MailHelper;
 import com.devesh.mailsender.api.service.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -19,9 +18,11 @@ public class MailController {
     MailService updateClickStatus;
     @Autowired
     MailHelper mailHelper;
+    @Autowired
+    CustomerCreateService customerCreateService;
 
     @GetMapping("/validate")
-    public ResponseEntity<String> validate(@RequestParam("uuid") UUID uuid) throws CustomException {
+    public ResponseEntity<String> validate(@RequestParam("uuid") UUID uuid) throws NotFoundException {
         updateClickStatus.updateClickStatus(uuid);
         return ResponseEntity.status(HttpStatus.OK).body("Active");
     }
@@ -30,5 +31,10 @@ public class MailController {
     public ResponseEntity<String> sendMail() {
         mailHelper.sendMail();
         return ResponseEntity.status(HttpStatus.OK).body("Mail Sent");
+    }
+    @PostMapping("/create")
+    public ResponseEntity<String> createCustomer(@RequestBody CustomerDto customerDto){
+        customerCreateService.createCustomer(customerDto);
+        return ResponseEntity.status(HttpStatus.OK).body("Customer created");
     }
 }
