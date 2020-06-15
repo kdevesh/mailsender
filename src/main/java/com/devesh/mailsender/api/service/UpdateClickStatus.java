36ab131803e98c5ce052e1878b2e6024f40edbfa
d0k026s <devesh.kumar@walmart.com>
2020-06-16 00:02:14 +0530

    Added Thank you mail functionality

--- src/main/java/com/devesh/mailsender/api/service/UpdateClickStatus.java
+++ src/main/java/com/devesh/mailsender/api/service/UpdateClickStatus.java
@@ -10,23 +10,35 @@ import org.springframework.stereotype.Service;
 import java.util.UUID;
 
 @Service("updateClickStatus")
-public class UpdateClickStatus implements MailService{
+public class UpdateClickStatus implements MailService {
     @Autowired
     CustomerRepository customerRepository;
+    @Autowired
+    MailService customMailSender;
+
     @Override
     public void updateClickStatus(UUID uuid) throws NotFoundException {
         Customer customer = customerRepository.findById(uuid).orElse(null);
-        if(customer==null)
-            throw new NotFoundException("Customer with uuid: "+uuid+" not found");
-        else
-        {
+        if (customer == null)
+            throw new NotFoundException("Customer with uuid: " + uuid + " not found");
+        else {
             customer.setHasClicked(true);
+            SimpleMailMessage simpleMailMessage = createSimpleMailMessage(customer);
+            sendMail(simpleMailMessage);
             customerRepository.save(customer);
         }
     }
 
+    private SimpleMailMessage createSimpleMailMessage(Customer customer) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(customer.getEmail());
+        message.setSubject("Thank you!!");
+        message.setText("Thank you!! " + customer.getFirstName() + " " + "for filling our survey!!");
+        return message;
+    }
+
     @Override
     public void sendMail(SimpleMailMessage simpleMailMessage) {
-
+        customMailSender.sendMail(simpleMailMessage);
     }
 }
