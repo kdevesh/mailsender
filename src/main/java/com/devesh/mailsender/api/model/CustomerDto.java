8594b5f7214366cd84d85eb8557634a317f0c7e5
d0k026s <devesh.kumar@walmart.com>
2020-06-15 23:47:19 +0530

    Created Customer DTO

---
+++ src/main/java/com/devesh/mailsender/api/model/CustomerDto.java
@@ -0,0 +1,22 @@
+package com.devesh.mailsender.api.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CustomerDto {
+
+	@JsonProperty(value = "firstName")
+	private String firstName;
+
+	@JsonProperty(value = "lastName")
+	private String lastName;
+
+	@JsonProperty(value = "email")
+	private String email;
+
+}
