3c3946f6ea63ca576bf6c37106ef77303713015c
d0k026s <devesh.kumar@walmart.com>
2020-06-15 23:46:42 +0530

    Updated Controller Advice

--- src/main/java/com/devesh/mailsender/api/exceptionhandler/EmailSenderExceptionHandler.java
+++ src/main/java/com/devesh/mailsender/api/exceptionhandler/EmailSenderExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.devesh.mailsender.api.exceptionhandler;
 
-import com.devesh.mailsender.api.exception.CustomException;
+import com.devesh.mailsender.api.exception.NotFoundException;
+import com.devesh.mailsender.api.exception.SaveException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,13 +12,18 @@ import javax.mail.SendFailedException;
 
 @ControllerAdvice
 public class EmailSenderExceptionHandler extends ResponseEntityExceptionHandler {
-    @ExceptionHandler(CustomException.class)
-    public ResponseEntity<String> handle(CustomException customException) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(customException.getMessage());
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(NotFoundException notFoundException) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(notFoundException.getMessage());
     }
 
     @ExceptionHandler(SendFailedException.class)
     public ResponseEntity<String> handleSendFailedException(SendFailedException sendFailedException) {
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Email could'nt be sent");
+    }
+
+    @ExceptionHandler(SaveException.class)
+    public ResponseEntity<String> handleSaveException(SaveException saveException) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(saveException.getMessage());
     }
 }
